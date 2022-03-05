b3f0f5ad15cae31d4f47b8e57707716b08d64161
frederik.steffen <frederik.steffen@gmail.com>
2022-03-05 17:19:58 +0100

    added prometheus values

--- modules/k8s/helm/prometheus/main.tf
+++ modules/k8s/helm/prometheus/main.tf
@@ -6,4 +6,5 @@ resource "helm_release" "prometheus" {
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "prometheus"
   version          = "15.0.2"
+  values           = ["${file("${path.module}/values.yml")}"]
 }
