e1d8dcacd050b889fc934ecf633b2b3db6c43359
frederik.steffen <frederik.steffen@gmail.com>
2022-04-09 21:32:59 +0200

    added more keycloak stuff

--- modules/k8s/keycloak/main.tf
+++ modules/k8s/keycloak/main.tf
@@ -50,7 +50,7 @@ data "kubernetes_secret" "postgres_credentials" {
 
 resource "kubernetes_secret" "keycloak_db" {
   metadata {
-    name = "keycloak-db-secret"
+    name      = "keycloak-db-secret"
     namespace = local.namespace
   }
 
@@ -71,5 +71,32 @@ resource "kubernetes_manifest" "keycloak" {
     kubernetes_manifest.keycloak_crd,
     kubernetes_manifest.keycloak_operator,
     kubernetes_secret.keycloak_db
+  ]
+}
+
+resource "kubernetes_manifest" "fdk_codes_realm" {
+  manifest   = yamldecode(file("${path.module}/keycloak_realm_fdk_codes.yaml"))
+  depends_on = [kubernetes_manifest.keycloak]
+}
+
+resource "kubernetes_manifest" "fdk_codes_notes_client" {
+  manifest   = yamldecode(file("${path.module}/keycloak_client_fdk_codes_notes.yaml"))
+  depends_on = [kubernetes_manifest.keycloak]
+}
+
+resource "random_password" "fdk_user_password" {
+  length = 16
+}
+
+resource "kubernetes_manifest" "fdk_codes_user" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/keycloak_user_fdk_codes.yaml.tftpl", {
+      "fdk_user_password" = random_password.fdk_user_password.result
+    }
+  ))
+
+  depends_on = [
+    kubernetes_manifest.keycloak,
+    random_password.fdk_user_password
   ]
 }
