9901319a0adcebca54b6ef597b83fef07818210d
frederik.steffen <frederik.steffen@gmail.com>
2022-04-22 20:21:51 +0200

    added oauth for notes webapp

--- modules/k8s/helm/istio/main.tf
+++ modules/k8s/helm/istio/main.tf
@@ -1,4 +1,9 @@
 
+locals {
+  internalKeycloakUrl = "http://keycloak-http.keycloak.svc.cluster.local"
+  keycloakUrl         = "https://iam.fdk.codes"
+}
+
 resource "helm_release" "istio-base" {
   namespace        = "istio-system"
   create_namespace = true
@@ -6,14 +11,7 @@ resource "helm_release" "istio-base" {
   repository       = var.istio.repository
   chart            = "base"
   version          = var.istio.version
-
-  dynamic "set" {
-    for_each = var.istio.values
-    content {
-      name  = set.key
-      value = set.value
-    }
-  }
+  values           = ["${file("${path.module}/istio-base-values.yml")}"]
 }
 
 resource "helm_release" "istiod" {
@@ -23,15 +21,8 @@ resource "helm_release" "istiod" {
   repository       = var.istio.repository
   chart            = "istiod"
   version          = var.istio.version
-
-  dynamic "set" {
-    for_each = var.istio.values
-    content {
-      name  = set.key
-      value = set.value
-    }
-  }
-  depends_on = [helm_release.istio-base]
+  values           = ["${file("${path.module}/istiod-values.yml")}"]
+  depends_on       = [helm_release.istio-base]
 }
 
 resource "helm_release" "istio_ingress" {
@@ -41,6 +32,7 @@ resource "helm_release" "istio_ingress" {
   repository       = var.istio.repository
   chart            = "gateway"
   version          = var.istio.version
+  values           = ["${file("${path.module}/gateway-values.yml")}"]
   depends_on       = [helm_release.istiod]
 }
 
@@ -49,4 +41,51 @@ resource "kubernetes_manifest" "istio_gateway" {
   for_each   = fileset(path.module, "gateway/*")
   manifest   = yamldecode(file("${path.module}/${each.value}"))
   depends_on = [helm_release.istio-base]
+}
+
+data "kubernetes_secret" "client_credentials_notes_webapp" {
+  metadata {
+    name      = "client-credentials-notes-webapp"
+    namespace = "keycloak"
+  }
+}
+
+resource "random_id" "hmac_key" {
+  byte_length = 32
+}
+
+resource "kubernetes_secret" "oauth_filter_credentials_notes_webapp" {
+  metadata {
+    name      = "oauth-filter-credentials-notes-webapp"
+    namespace = "default"
+  }
+  data = {
+    "token-secret.yaml" = "${templatefile("${path.module}/filter/envoy-token-secret.yml.tftpl", {
+      "client_secret" = data.kubernetes_secret.client_credentials_notes_webapp.data.clientSecret
+    })}"
+    "hmac-secret.yaml" = "${templatefile("${path.module}/filter/envoy-hmac-secret.yml.tftpl", {
+      "hmac_secret" = random_id.hmac_key.b64_std
+    })}"
+  }
+
+  depends_on = [
+    data.kubernetes_secret.client_credentials_notes_webapp,
+    random_id.hmac_key
+  ]
+}
+
+resource "kubernetes_manifest" "oauth_filter_notes_webapp" {
+  manifest = yamldecode(templatefile(
+    "${path.module}/filter/notes-webapp-oauth.yml.tftpl", {
+      "client_id"      = data.kubernetes_secret.client_credentials_notes_webapp.data.clientId,
+      "token_endpoint" = "${local.internalKeycloakUrl}/auth/realms/fdk-codes/protocol/openid-connect/token",
+      "auth_endpoint"  = "${local.keycloakUrl}/auth/realms/fdk-codes/protocol/openid-connect/auth",
+    }
+  ))
+
+  depends_on = [
+    helm_release.istio_ingress,
+    data.kubernetes_secret.client_credentials_notes_webapp,
+    kubernetes_secret.oauth_filter_credentials_notes_webapp
+  ]
 }
--- modules/k8s/helm/keycloak/main.tf
+++ modules/k8s/helm/keycloak/main.tf
@@ -96,3 +96,42 @@ resource "keycloak_realm" "fdk_codes" {
 
   depends_on = [helm_release.keycloak]
 }
+
+resource "keycloak_openid_client" "oidc_client_notes_webapp" {
+  realm_id              = keycloak_realm.fdk_codes.id
+  client_id             = "notes-web-app"
+  name                  = "notes-web-app"
+  access_type           = "CONFIDENTIAL"
+  standard_flow_enabled = true
+  valid_redirect_uris = [
+    "https://notes.fdk.codes/*"
+  ]
+  depends_on = [keycloak_realm.fdk_codes]
+}
+
+resource "kubernetes_secret" "client_credentials_notes_webapp" {
+  metadata {
+    name      = "client-credentials-notes-webapp"
+    namespace = local.namespace
+  }
+
+  data = {
+    clientId     = keycloak_openid_client.oidc_client_notes_webapp.client_id
+    clientSecret = keycloak_openid_client.oidc_client_notes_webapp.client_secret
+  }
+
+  depends_on = [keycloak_openid_client.oidc_client_notes_webapp]
+}
+
+# TODO fixme
+resource "keycloak_user" "fdk" {
+  realm_id       = keycloak_realm.fdk_codes.id
+  email          = "fdk@fdk.codes"
+  email_verified = true
+  username       = "fdk"
+  initial_password {
+    value     = "1234"
+    temporary = false
+  }
+  depends_on = [keycloak_realm.fdk_codes]
+}
